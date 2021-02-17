abdc9b25bc6a6061db0784a65de8ff618fdd5e71
ahmadirfaan <irfaan.hibatullah@gmail.com>
2021-02-17 16:59:18 +0700

    Result Completed But UI is damaged

--- app/src/main/java/com/pascal/beamvolume/MainActivity.kt
+++ app/src/main/java/com/pascal/beamvolume/MainActivity.kt
@@ -26,12 +26,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         editWidth = findViewById(R.id.inputlebar)
         editHeight = findViewById(R.id.inputTinggi)
         editLength = findViewById(R.id.inputPanjang)
+        btnCalculate = findViewById(R.id.materialButtonResult)
+        tvResult = findViewById(R.id.resultTextView)
         btnCalculate.setOnClickListener(this)
 
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT) as String
             tvResult.text = result
         }
+    }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.materialButtonResult) {
+            val inputWidth = editWidth.text.toString().trim()
+            val inputHeight = editHeight.text.toString().trim()
+            val inputLength = editLength.text.toString().trim()
+
+            var isEmptyField = false
+
+            if(inputWidth.isEmpty()) {
+                isEmptyField = true
+                editWidth.error = "Harap masukkan lebar dari Balok"
+            }
+            if(inputHeight.isEmpty()) {
+                isEmptyField = true
+                editHeight.error = "Harap masukkan tinggi dari Balok"
+            }
+            if(inputLength.isEmpty()) {
+                isEmptyField = true
+                editLength.error = "Harap masukkan panjang dari Balok"
+            }
+
+            if(!isEmptyField) {
+                val volume = inputWidth.toDouble() * inputHeight.toDouble() * inputLength.toDouble()
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
     }
 
 
