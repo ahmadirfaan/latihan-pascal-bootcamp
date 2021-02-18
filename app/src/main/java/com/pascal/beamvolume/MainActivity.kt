6fb5a9ef5f893d6b1d773182b8bf9ee889e68051
ahmadirfaan <irfaan.hibatullah@gmail.com>
2021-02-18 13:14:59 +0700

    Add Fragment Two

--- app/src/main/java/com/pascal/beamvolume/MainActivity.kt
+++ app/src/main/java/com/pascal/beamvolume/MainActivity.kt
@@ -9,50 +9,9 @@ import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var userNameText : TextInputEditText
-    private val activityName = "MAIN ACTIVITY"
-
-    companion object {
-        const val USER_NAME = "USER_NAME"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        userNameText = findViewById(R.id.username_text_input)
-    }
-
-    fun moveToSecondActivity(view: View) {
-        val intent = Intent(this, BoardActivity::class.java)
-        intent.putExtra(USER_NAME, userNameText.text.toString())
-        startActivity(intent)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i(activityName, "onStart() CALLED")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i(activityName, "onResume() CALLED")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i(activityName, "onPause() CALLED")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(activityName, "onDestroy() CALLED")
-
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i(activityName, "OnRestart() CALLED")
     }
 
 
